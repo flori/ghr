61a727ffb5463a899e5657075b3e317611366f1b
Florian Frank <flori@ping.de>
2025-08-31 05:07:11 +0200

    Remove duplicate perform method from GithubReleaseJIRANotifier

--- app/models/github_release_jira_notifier.rb
+++ app/models/github_release_jira_notifier.rb
@@ -33,20 +33,4 @@ class GithubReleaseJIRANotifier
     @github_release.save!
     @github_release
   end
-
-  def perform
-    @github_release.pending_notifier_jira? or return
-    if JIRAClient.configured?
-      JIRAClient.issue!(
-        summary: summary,
-        description: description
-      )
-      Rails.logger.info "Created an issue for #{release_url.inspect}"
-    else
-      Rails.logger.info "JIRA is not configured omitting JIRA notification for #{release_url.inspect}"
-    end
-    @github_release.pending_notifier_jira = false
-    @github_release.save!
-    @github_release
-  end
 end
